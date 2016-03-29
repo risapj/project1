99c0011c7c581d699d62cc1d4d436a25a2ace540
Ubuntu <rap2181@columbia.edu>
2016-03-29 13:26:31 +0000

    idk but whatever

--- webserver/TRserver.py
+++ webserver/TRserver.py
@@ -19,6 +19,8 @@ import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
+import jinja2
+
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -33,15 +35,11 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 #     postgresql://USER:PASSWORD@w4111db.eastus.cloudapp.azure.com/username
 #
-# For example, if you had username ewu2493, password foobar, then the following line would be:
+# For example, if you had username ewu2493, password foobar, then the following line would be
 #
 #     DATABASEURI = "postgresql://ewu2493:foobar@w4111db.eastus.cloudapp.azure.com/ewu2493"
 #
-
-DATABASEURI = "sqlite:///test.db"
-
-#DATABASEURI = "postgres://trw2121:JVTTMM@w4111db.eastus.cloudapp.azure.com/trw2121"
-
+DATABASEURI = "postgresql://trw2121:JVTTMM@w4111db.eastus.cloudapp.azure.com/trw2121"
 
 #
 # This line creates a database engine that knows how to connect to the URI above
@@ -63,14 +61,6 @@ engine = create_engine(DATABASEURI)
 #     .schema <tablename>   -- print CREATE TABLE statement for table
 # 
 # The setup code should be deleted once you switch to using the Part 2 postgresql database
-#
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-#
 # END SQLITE SETUP CODE
 #
 
@@ -118,29 +108,35 @@ def teardown_request(exception):
 # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 #
 @app.route('/')
-def index():
-  """
-  request is a special object that Flask provides to access web request information:
+def mainpage():
+  ddmenu = g.conn.execute("SELECT * FROM Movie")
+  movies = []
+  for result in ddmenu:
+    movies.append(result['title'].encode('utf-8'))
+  
+#  context = dict(data = titles)
+ 
+  return render_template("mainpage.html", data = movies)
+#def index():
+#  """
+#  request is a special object that Flask provides to access web request information:
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+#  request.method:   "GET" or "POST"
+#  request.form:     if the browser submitted a form, this contains the data in the form
+#  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
 
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
+#  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+#  """
 
   # DEBUG: this is debugging code to see what request looks like
-  print request.args
+#  print request.args
 
 
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
+
+  
 
   #
   # Flask uses Jinja templates, which is an extension to HTML where you can
@@ -168,14 +164,14 @@ def index():
   #     <div>{{n}}</div>
   #     {% endfor %}
   #
-  context = dict(data = names)
+#  context = dict(data = titles)
 
 
   #
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html", **context)
+#  return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at
@@ -185,10 +181,82 @@ def index():
 # notice that the functio name is another() rather than index()
 # the functions for each app.route needs to have different names
 #
-@app.route('/another')
-def another():
-  return render_template("anotherfile.html")
 
+@app.route('/danishgirl')
+def danishgirl():
+  #dg = g.conn.execute("SELECT * FROM Movie M, Acts_in A, Directs D, Writes W, Nominated_p Np, Nominated_m Nm WHERE M.mid = 3")
+  #dg = g.conn.execute("SELECT * FROM Movie M, Acts_in A WHERE M.mid = 3")
+
+#below does no work, middle dg does. 
+  dg = g.conn.execute("SELECT * FROM Person P WHERE P.pid IN (SELECT * FROM Acts_in  Movie M3))")
+  dginfo = dg.fetchall()
+  
+  return render_template("danishgirl.html", data = dginfo)
+
+
+
+
+@app.route('/movie-search')
+def moviesearch():
+  
+#get everything from Movie table
+  movies = g.conn.execute("SELECT * FROM Movie;")
+  titles = movies.fetchall()
+
+  context = dict(data = titles)
+
+  return render_template("movie-search.html", data = titles)
+
+'''
+@app.route('/movie-search/<search>')
+def search(search=None):
+  movies = g.conn.cursor()
+  movies.execute("SELECT DISTINCT title FROM Movie;")
+  movies = movies.fetchall();
+
+  if title == "All":
+    title = None
+
+  title = request.args.get('Movie')
+  movieinfo = g.conn.cursor()
+  director = g.conn.cursor()
+  writer = g.conn.cursor()
+  actchar = g.conn.cursor()
+  award = g.conn.cursor()
+
+  if title == "All":
+    title = None
+
+  if title:
+    movieinfo.execute("SELECT DISTINCT M.mid, M.title, M.length, M.language, M.release_date, M.production_co, M.genre FROM Movie M WHERE M.title =\'" + title + "\'")
+    director.execute("SELECT DISTINCT P.name FROM Movie M, Directs D, Person P WHERE M.title =\'" + title + "\' AND M.mid = D.mid AND D.pid = P.pid")
+    writer.execute("SELECT DISTINCT P.name FROM Movie M, Writes W, Person P WHERE M.title =\'" + title + "\' AND M.mid = W.mid AND W.pid = P.pid")
+    actchar.execute("SELECT DISTINCT P.name, A.char_name FROM Person P, Acts_in A, Movie M WHERE M.title =\'" + title + "\' AND M.mid = A.mid AND A.pid = P.pid")
+    award.execute("SELECT A.aid, A.year, A.type, A.category FROM Awards A, Nominated_p Np, Nominated_m Nm, Movie M WHERE M.title =\'")
+
+  final_results = []
+  for result in results:
+    current_movie = {}
+    #movieinfo =  
+'''
+
+
+#  return render_template("movie-search.html", **context)
+
+
+
+@app.route('/person-search')
+def personsearch():
+  return render_template("person-search.html")
+
+
+@app.route('/award-search')
+def awardsearch():
+  return render_template("award-search.html")
+
+@app.route('/character-search')
+def charactersearch():
+  return render_template("character-search.html")
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
