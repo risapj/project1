a28cbff87c12296b605aeb9b4ede72e5154e7cee
Ubuntu <rap2181@columbia.edu>
2016-03-31 06:39:48 +0000

    ....

--- webserver/TRserver.py
+++ webserver/TRserver.py
@@ -47,20 +47,7 @@ def teardown_request(exception):
 def mainpage():
   return render_template("mainpage.html")
 
-@app.route('/danishgirl')
-def danishgirl():
-  #dg = g.conn.execute("SELECT * FROM Movie M, Acts_in A, Directs D, Writes W, Nominated_p Np, Nominated_m Nm WHERE M.mid = 3")
-  #dg = g.conn.execute("SELECT * FROM Movie M, Acts_in A WHERE M.mid = 3")
 
-#below does no work, middle dg does. 
-  dg = g.conn.execute("SELECT * FROM Person P WHERE P.pid IN (SELECT * FROM Acts_in  Movie M3))")
-  dginfo = dg.fetchall()
-  
-  return render_template("danishgirl.html", data = dginfo)
-
-
-#@app.route('/movie-search', methods=['GET', 'POST'])
-#@app.route('/movie-search<search>', methods=['GET', 'POST'])
 @app.route('/movie-search')
 @app.route('/movie-search<search>')
 def moviesearch(search=None):
@@ -82,24 +69,8 @@ def moviesearch(search=None):
 
   if title:  
     results = g.conn.execute("SELECT * FROM Movie M WHERE M.title = \'"+title +"\'")
-#    people = g.conn.execute("SELECT P.name, A.char_name FROM Movie M, Person P, Acts_in A WHERE M.title = \'"+title+"\' AND M.mid = A.mid AND A.pid=P.pid")
-#    director = g.conn.execute("SELECT P.name FROM Movie M, Person P, Directs D WHERE M.title = \'"+title+"\' AND M.mid = D.mid AND D.pid = P.pid")
-#    writer = g.conn.execute("SELECT P.name FROM Movie M, Person P, Writes W WHERE M.title = \'"+title+"\' AND M.mid = W.mid AND W.pid = P.pid")
-#    awards = g.conn.execute("SELECT A.year, A.type, A.category, P.name, N.won FROM Awards A, Nominated_p N, Person P, Movie M WHERE M.title = \'"+title+"\' AND M.mid=N.mid AND N.pid=P.pid AND N.aid=A.aid")
-#    awardm = g.conn.execute("SELECT A.year, A.type, A.category, N.won FROM Awards A, Nominated_m N, Movie M WHERE M.title = \'"+title+"\' AND M.mid=N.mid AND N.aid=A.aid")
-#    review = g.conn.execute("SELECT R.username, R.text, R.post_time FROM Review_posted R, Movie M WHERE M.title = \'"+title+"\' AND R.mid = M.mid")
-#    user = g.conn.execute("SELECT * FROM Users")
-#    rating = g.conn.execute("SELECT R.username, R.rating, R.post_time FROM Rating_rated R, Movie M WHERE M.title = \'"+title+"\' AND M.mid = R.mid")
   else:
     results = g.conn.execute("SELECT * FROM Movie M")
-#    people = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    director = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    writer = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    awards = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    awardm = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    review = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    user = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
-#    rating = g.conn.execute("SELECT P.name FROM Person P WHERE P.pid = 100")
 
   people = g.conn.execute("SELECT P.pid, P.name, A.char_name, A.mid FROM Person P, Acts_in A WHERE A.pid=P.pid")
   director = g.conn.execute("SELECT P.name, D.pid, D.mid FROM Person P, Directs D WHERE D.pid = P.pid")
@@ -169,50 +140,13 @@ def moviesearch(search=None):
     current_movie["ratings"] = rating_array
     final_results.append(current_movie)
 
-  return render_template('movie-search.html', data = movies, data2 = minfo, title = title, results = final_results)
-  #return render_template("movie-search.html", data = movies, data2 = minfo, title = title)
-
-'''
-@app.route('/movie-search/<search>')
-def search(search=None):
-  movies = g.conn.cursor()
-  movies.execute("SELECT DISTINCT title FROM Movie;")
-  movies = movies.fetchall();
-
-  if title == "All":
-    title = None
-
-  title = request.args.get('Movie')
-  movieinfo = g.conn.cursor()
-  director = g.conn.cursor()
-  writer = g.conn.cursor()
-  actchar = g.conn.cursor()
-  award = g.conn.cursor()
-
-  if title == "All":
-    title = None
-
-  if title:
-    movieinfo.execute("SELECT DISTINCT M.mid, M.title, M.length, M.language, M.release_date, M.production_co, M.genre FROM Movie M WHERE M.title =\'" + title + "\'")
-    director.execute("SELECT DISTINCT P.name FROM Movie M, Directs D, Person P WHERE M.title =\'" + title + "\' AND M.mid = D.mid AND D.pid = P.pid")
-    writer.execute("SELECT DISTINCT P.name FROM Movie M, Writes W, Person P WHERE M.title =\'" + title + "\' AND M.mid = W.mid AND W.pid = P.pid")
-    actchar.execute("SELECT DISTINCT P.name, A.char_name FROM Person P, Acts_in A, Movie M WHERE M.title =\'" + title + "\' AND M.mid = A.mid AND A.pid = P.pid")
-    award.execute("SELECT A.aid, A.year, A.type, A.category FROM Awards A, Nominated_p Np, Nominated_m Nm, Movie M WHERE M.title =\'")
-
-  final_results = []
-  for result in results:
-    current_movie = {}
-    #movieinfo =  
-'''
-
-
-#  return render_template("movie-search.html", **context)
-
+  return render_template('movie-search.html', data = movies, data2 = minfo, chosen_title = title, title = title, results = final_results)
 
 
 @app.route('/person-search')
-def personsearch():
-#get all info from Movie
+@app.route('/person-search<search>')
+def personsearch(search=None):
+#get all info from Person
   forTable = g.conn.execute("SELECT * FROM Person")
   pinfo = forTable.fetchall()
 
@@ -222,16 +156,219 @@ def personsearch():
   for result in ddmenu:
     people.append(result['name'].encode('utf-8'))
 
-  return render_template("person-search.html", data = people, data2 = pinfo)
+  name = request.args.get('name')
+
+  if name == "All":
+    name = None
+  if name:
+    results = g.conn.execute("SELECT * FROM Person P WHERE P.name = \'"+name+"\'")
+  else:
+    results = g.conn.execute("SELECT * FROM Person P")
+
+  moviechar = g.conn.execute("SELECT A.pid, A.char_name, M.title, M.release_date, M.mid FROM Acts_in A, Movie M WHERE A.mid=M.mid")
+  director = g.conn.execute("SELECT D.pid, M.title, M.release_date, M.mid FROM Directs D, Movie M WHERE D.mid=M.mid")
+  writer = g.conn.execute("SELECT W.pid, M.title, M.release_date, M.mid FROM Writes W, Movie M WHERE W.mid=M.mid")
+  awards = g.conn.execute("SELECT N.pid, A.year, A.type, A.category, N.won, M.title FROM Nominated_p N, Awards A, Movie M WHERE A.aid=N.aid AND N.mid=M.mid")
+
+  final_results = []
+  for result in results:
+    current_person = {}
+    current_person["info"] = [result[1], str(result[2]), result[3], result[4]]
+    person_id = str(result[0])
+    moviechar_array = []
+    for mc in  moviechar:
+      if person_id == str(mc[0]):
+        moviechar_array.append([mc[1].encode('utf-8'), mc[2].encode('utf-8'), mc[3]])
+    current_person["moviechar"] = moviechar_array
+    director_array = []
+    for d in director:
+      if person_id == str(d[0]):
+        director_array.append([d[1].encode('utf-8'), d[2]])
+    current_person["director"] = director_array
+    writer_array = []
+    for w in writer:
+      if person_id == str(w[0]):
+        writer_array.append([w[1].encode('utf-8'), w[2]])
+    current_person["writer"] = writer_array
+    award_array = []
+    for a in awards:
+      if person_id == str(a[0]):
+        won = str(a[4])
+        won2 = "t"
+        if won == won2:
+          win = 'Won'
+        else:
+          win = 'Nominated'
+        award_array.append([a[5].encode('utf-8'), a[1], a[2].encode('utf-8'), a[3].encode('utf-8'), win])
+    current_person["award"] = award_array
+    final_results.append(current_person)
+
+  return render_template("person-search.html", name = name, chosen_name = name, data = people, data2 = pinfo, results= final_results)
 
 
 @app.route('/award-search')
-def awardsearch():
-  return render_template("award-search.html")
+@app.route('/award-search<search>')
+def index(search=None):
+
+  ay = []
+  awardYear = g.conn.execute("SELECT DISTINCT A.year FROM Awards A")
+  for year in awardYear:
+    ay.append(str(year[0]).encode('utf-8'))
+
+  awardType = g.conn.execute("SELECT DISTINCT A.type FROM Awards A")
+  at = []
+  for ty in awardType:
+    at.append(ty[0].encode('utf-8'))
+
+  awardCat = g.conn.execute("SELECT DISTINCT A.category FROM Awards A")
+  ac = []
+  for result in awardCat:
+    ac.append(result[0].encode('utf-8'))
+
+  year =  request.args.get('year')
+  atype =  request.args.get('atype')
+  category = request.args.get('category')
+
+  if year == "All":
+    year = None
+  if atype == "All":
+    atype = None
+  if category == "All":
+    category = None
+
+
+  if atype and category and year:
+    results= g.conn.execute("SELECT * FROM Awards A WHERE A.type=\'" + atype + " \' AND A.category = \'" + category + "\' AND A.year =\'"+year+"\'")
+  elif atype:
+    results=g.conn.execute("SELECT * FROM Awards A WHERE A.type = \'" + atype + "\'")
+  elif category:
+    results=g.conn.execute("SELECT * FROM Awards A WHERE A.category=\'" + category + "\'")
+  elif atype and category:
+    results= g.conn.execute("SELECT * FROM Awards A WHERE A.type=\'" + atype + " \' AND A.category = \'" + category + "\'")
+  elif atype and year:
+    results= g.conn.execute("SELECT * FROM Awards A WHERE A.type=\'" + atype + " \' AND A.year =\'"+year+"\'")
+  elif category and year:
+    results= g.conn.execute("SELECT * FROM Awards A WHERE A.category = \'" + category + "\' AND A.year =\'"+year+"\'")
+  else:
+    results=g.conn.execute("SELECT * FROM Awards A")
+
+  moviesp = g.conn.execute("SELECT M.mid, M.title, P.name, N.aid, N.won FROM Movie M, Person P, Nominated_p N WHERE N.mid=M.mid AND N.pid=P.pid")
+  moviesm = g.conn.execute("SELECT M.mid, M.title, N.aid, N.won FROM Movie M, Nominated_m N WHERE N.mid=M.mid")
+
+  final_results = []
+  for result in results:
+    current_award = {}
+    current_award["info"] = [result[1], result[2], result[3]]
+    aid = str(result[0])
+    mp_array = []
+    for mp in moviesp:
+      if aid == str(mp[3]):
+        won = str(mp[4])
+        won2 = "t"
+        if won ==won2:
+          win = 'Won'
+        else:
+          win = 'Nominated'
+        mp_array.append([mp[1].encode('utf-8'), mp[2].encode('utf-8'), win])
+    current_award["moviep"] = mp_array
+    m_array = []
+    for m in moviesm:
+      if aid == str(m[2]):
+        won = str(m[3])
+        won2 = "t"
+        if won==won2:
+          win = 'Won'
+        else:
+          win = 'Nominated'
+        m_array.append([m[1].encode('utf-8'), win])
+    current_award["moviem"] = m_array
+    final_results.append(current_award) 
+
+  return render_template('award-search.html', awardYear = ay, awardType=at, awardCat=ac, chosen_year = ay, chosen_type = at, chosen_cat = ac, results=final_results)
+
+@app.route('/user-search')
+@app.route('/user-search<search>')
+def usersearch(search=None):
+#get all info from User
+  forTable = g.conn.execute("SELECT * FROM Users")
+  uinfo = forTable.fetchall()
+
+#get info from User, create array, put usernames in array
+  ddmenu = g.conn.execute("SELECT * FROM Users")
+  user = []
+  for result in ddmenu:
+    user.append(result['username'].encode('utf-8'))
+
+  username = request.args.get('username')
+
+  if username == "All":
+    name = None
+  if username:
+    results = g.conn.execute("SELECT * FROM Users U WHERE U.username = \'"+username+"\'")
+  else:
+    results = g.conn.execute("SELECT * FROM Users U")
+
+  review = g.conn.execute("SELECT R.username, M.title, R.rev_text, R.post_date FROM Movie M, Review_posted R WHERE R.mid = M.mid")
+  rating = g.conn.execute("SELECT R.username, M.title, R.rating, R.post_date FROM Movie M, Rating_rated R WHERE R.mid = M.mid")
+
+  final_results = []
+  for result in results:
+    current_user = {}
+    current_user["info"] = [result[0], result[1], result[2], result[3]]
+    uname = result[0]
+    review_array = []
+    for rev in review:
+      if uname == str(rev[0]):
+        review_array.append([rev[1].encode('utf-8'), rev[2].encode('utf-8'), rev[3]])
+    current_user["review"] = review_array
+    rating_array = []
+    for rat in rating:
+      if uname == str(rat[0]):
+        rating_array.append([rat[1].encode('utf-8'), rat[2], rat[3]])
+    current_user["rating"] = rating_array
+    final_results.append(current_user)
+  
+  return render_template("user-search.html", username = username, chosen_username = username, data = user, data2 = uinfo, results=final_results)
+
 
 @app.route('/character-search')
-def charactersearch():
-  return render_template("character-search.html")
+@app.route('/character-search<search>')
+def characterearch(search=None):
+#get all info from Character
+  forTable = g.conn.execute("SELECT * FROM Character")
+  cinfo = forTable.fetchall()
+
+  char_name = request.args.get('char_name')
+
+  if char_name == "All":
+    char_name = None
+
+  if char_name:
+    results = g.conn.execute("SELECT * FROM Character C WHERE C.char_name = \'"+char_name+"\'")
+  else:
+    results = g.conn.execute("SELECT * FROM Character C")
+
+  moviechar = g.conn.execute("SELECT A.pid, A.char_name, M.mid, M.title, M.length, M.language, M.release_date, M.production_co, M.genre FROM Acts_in A, Movie M WHERE A.mid=M.mid")
+  actors = g.conn.execute("SELECT P.pid, P.name, A.char_name, A.mid FROM Person P, Acts_in A WHERE A.pid=P.pid") 
+
+  final_results = []
+  for result in results:
+    current_char = {}
+    current_char["info"] = [result[0]]
+    current_movie = []
+    current_actor = []
+    for actor in actors:
+      if actor[2] == result[0]:
+        current_actor.append(actor[1])
+    current_char["person_info"] = current_actor
+    for mc in moviechar:
+      if mc[1] == result[0]:
+        current_movie.append([mc[3]])
+    current_char["movie_info"] = current_movie
+    final_results.append(current_char)
+
+  return render_template("/character-search.html", cinfo = cinfo, results = final_results)
+
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
--- webserver/static/action.js
+++ webserver/static/action.js
@@ -9,23 +9,47 @@ $(document).ready(function() {
     });
 
 // switch the status of display
-function trigger(element) {
+  function trigger(element) {
     if (element.css('display') !== 'none') {
       element.hide();
     } else {
       element.show();
     }
   }
-
+    $("#submit-award").click(function(){
+	var year = getInnerHTML("year");
+	var atype = getInnerHTML("atype");
+	var category = getInnerHTML("category");
+        var url = 'http://104.41.144.56:8111/award-search';
+	window.location.href = url + "?year="+year+"&atype="+atype+"&category="+category;
+    });
 
     $("#submit").click(function(){
-    	var title = getInnerHTML("title");
-	var url = 'http://13.82.29.208:8111/movie-search';
-      window.location.href = url + '?title=' + title;
+	var title = getInnerHTML("title");
+	var url = 'http://104.41.144.56:8111/movie-search';
+	window.location.href = url + '?title=' + title;
+    });
+
+    $("#submit-person").click(function(){
+    	var name = getInnerHTML("name");
+	var url = 'http://104.41.144.56:8111/person-search';
+        window.location.href = url + '?name=' + name;
+    });
+
+    $("#submit-char").click(function(){
+        var char_name = getInnerHTML("char");
+        var url = 'http://104.41.144.56:8111/character-search';
+        window.location.href = url + '?char=' + char_name;
+    });
+
+    $("#submit-username").click(function(){
+        var username = getInnerHTML("username");
+        var url = 'http://104.41.144.56:8111/user-search';
+        window.location.href = url + '?username=' + username;
     });
 });
 
 function getInnerHTML(input){
 	var value = document.getElementById(input);
 	return value[value.selectedIndex].innerHTML;
-}
+}k
